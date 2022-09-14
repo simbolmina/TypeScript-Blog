30b0d11f17df97a610261d1e077d0337ca3fe368
Bilal <simbolmina@hotmail.com>
2022-09-14 13:24:53 +0300

    added horizontal resizable bar

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -16,10 +16,12 @@ function CodeCell() {
   return (
     <Resizable direction="vertical">
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
-        <CodeEditor
-          initialValue="console.log('test')"
-          onChange={(value) => setInput(value)}
-        />
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="console.log('test')"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./Resizable.css";
 
 interface ResizableProps {
@@ -7,17 +7,27 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      height={300}
-      width={Infinity}
-      resizeHandles={["s"]}
-      maxConstraints={[Infinity, window.innerHeight * 0.7]}
-      minConstraints={[Infinity, 60]}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      height: 300,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ["e"],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+    };
+  } else {
+    resizableProps = {
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+      maxConstraints: [Infinity, window.innerHeight * 0.7],
+      minConstraints: [Infinity, 60],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
