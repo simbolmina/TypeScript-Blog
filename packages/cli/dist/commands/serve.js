0cbc06dae279a27b9b7320ae2761feb31f4d2722
simbolmina <simbolmina@hotmail.com>
2022-10-01 19:19:47 +0300

    cli is created and added

---
+++ packages/cli/dist/commands/serve.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serveCommand = void 0;
+const path_1 = __importDefault(require("path"));
+const commander_1 = require("commander");
+const local_api_1 = require("local-api");
+exports.serveCommand = new commander_1.Command()
+    .command("serve [filename]")
+    //[filename] is option cuz of []
+    .description("Open a file for editing")
+    .option("-p, --port <number>", "port to run server on", "4005")
+    //<number> is required cuz of <>
+    .action((filename = "notebook.js", options) => {
+    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+    (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir);
+});
