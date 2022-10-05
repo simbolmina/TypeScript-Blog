e1dec7f1c6666c85ef901e4df2e8551972ca61a7
Bilal <simbolmina@hotmail.com>
2022-10-05 12:15:30 +0300

    added functionality to local-api

--- packages/local-client/src/App.tsx
+++ packages/local-client/src/App.tsx
@@ -1,12 +1,8 @@
-// import CodeCell from "./components/CodeCell";
 import CellList from "./components/CellList";
-// import TextEditor from "./components/TextEditor";
 
 function App() {
   return (
     <div>
-      {/* <CodeCell /> */}
-      {/* <TextEditor /> */}
       <CellList />
     </div>
   );
